6003e38750f99f9e1bb73f610a9dcafcc30f1cdc
Denis Sandal <den.sandal@gmail.com>
2018-12-11 02:18:31 +0700

    Генерация заготовок
    - модели
    - миграции
    - фабрики
    - сидеры

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(LanguagesTableSeeder::class);
+        $this->call(PagesTableSeeder::class);
+        $this->call(BlogSeeder::class);
     }
 }
