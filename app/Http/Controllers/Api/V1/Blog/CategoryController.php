72e47613becd4fa1c1aca89db2c02457caec6947
Denis Sandal <den.sandal@gmail.com>
2018-12-11 22:59:34 +0700

    GET api/v1/blog/categories

--- app/Http/Controllers/Api/V1/Blog/CategoryController.php
+++ app/Http/Controllers/Api/V1/Blog/CategoryController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Api\V1\Blog;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Blog\CategoryResource;
+use App\Models\Blog\Category;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        //
+        $categories = Category::all();
+
+        return CategoryResource::collection($categories);
     }
 }
--- app/Http/Resources/Blog/CategoryResource.php
+++ app/Http/Resources/Blog/CategoryResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Blog;
 
+use App\Models\Blog\Category;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CategoryResource extends JsonResource
@@ -14,6 +15,12 @@ class CategoryResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        /** @var JsonResource|Category $this */
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+        ];
     }
 }
