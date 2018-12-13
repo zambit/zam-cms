6fa9bf49fdf845a89936892de22585feaabfc6f1
Denis Sandal <den.sandal@gmail.com>
2018-12-14 00:02:18 +0700

    CRUD для текстовых страниц

---
+++ app/Http/Sections/Pages.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Sections;
+
+use SleepingOwl\Admin\Contracts\Display\DisplayInterface;
+use SleepingOwl\Admin\Contracts\Form\FormInterface;
+use SleepingOwl\Admin\Contracts\Initializable;
+use SleepingOwl\Admin\Section;
+
+/**
+ * Class Pages
+ *
+ * @property \App\Models\Page $model
+ *
+ * @see http://sleepingowladmin.ru/docs/model_configuration_section
+ */
+class Pages extends Section implements Initializable
+{
+    /**
+     * @see http://sleepingowladmin.ru/docs/model_configuration#ограничение-прав-доступа
+     *
+     * @var bool
+     */
+    protected $checkAccess = false;
+
+    /**
+     * @var string
+     */
+    protected $title = 'Text Pages';
+
+    /**
+     * @var string
+     */
+    protected $alias;
+
+    protected $icon = 'fa fa-edit';
+
+    public function initialize()
+    {
+        $this->addToNavigation($priority = 5);
+    }
+
+    /**
+     * @return DisplayInterface
+     */
+    public function onDisplay()
+    {
+        $display = \AdminDisplay::table()
+            ->setHtmlAttribute('class', 'table-primary')
+            ->setColumns(
+                \AdminColumn::text('id', '#')->setWidth('30px'),
+                \AdminColumn::text('title', 'Title'),
+                \AdminColumn::datetime('created_at', 'Created')
+                    ->setFormat('d.m.Y H:i')->setWidth('150px')
+            );
+
+        return $display->paginate(100);
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return FormInterface
+     */
+    public function onEdit($id)
+    {
+        $panelEn = \AdminForm::panel()->addBody([
+            \AdminFormElement::text('title', 'Title')
+                ->addValidationRule('max:255')
+                ->required(),
+            \AdminFormElement::textarea('description', 'Description')
+                ->required(),
+            \AdminFormElement::textarea('keywords', 'Keywords')
+                ->addValidationRule('max:255')
+                ->required(),
+            \AdminFormElement::textarea('content', 'Content')
+                ->required(),
+        ]);
+
+        $tabs = \AdminDisplay::tabbed();
+
+        $tabs->appendTab($panelEn, 'English');
+
+        return $tabs;
+    }
+
+    /**
+     * @return FormInterface
+     */
+    public function onCreate()
+    {
+        return $this->onEdit(null);
+    }
+
+    /**
+     * @return void
+     */
+    public function onDelete($id)
+    {
+        // remove if unused
+    }
+
+    /**
+     * @return void
+     */
+    public function onRestore($id)
+    {
+        // remove if unused
+    }
+}
--- app/Providers/AdminSectionsServiceProvider.php
+++ app/Providers/AdminSectionsServiceProvider.php
@@ -13,6 +13,7 @@ class AdminSectionsServiceProvider extends ServiceProvider
     protected $sections = [
         \App\Models\User::class => 'App\Http\Sections\Users',
         \App\Models\Language::class => 'App\Http\Sections\Languages',
+        \App\Models\Page::class => 'App\Http\Sections\Pages',
         \App\Models\Blog\Category::class => 'App\Http\Sections\Blog\Categories',
         \App\Models\Blog\Tag::class => 'App\Http\Sections\Blog\Tags',
         \App\Models\Blog\Article::class => 'App\Http\Sections\Blog\Posts',
