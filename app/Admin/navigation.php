cf04334be288a0ae534a41d86e5cb2b39c452b7f
Denis Sandal <den.sandal@gmail.com>
2018-12-16 00:02:15 +0700

    Logout

--- app/Admin/navigation.php
+++ app/Admin/navigation.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-
+    [
+        'title' => 'Logout',
+        'icon' => 'fa fa-arrow-right',
+        'url' => route('admin.logout'),
+    ],
 ];
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -3,3 +3,11 @@
 Route::get('', ['as' => 'admin.dashboard', function () {
     return response()->redirectTo('/admin/articles');
 }]);
+
+Route::get('logout', ['as' => 'admin.logout', function () {
+    \Auth::guard()->logout();
+
+    request()->session()->invalidate();
+
+    return response()->redirectTo('/');
+}]);
